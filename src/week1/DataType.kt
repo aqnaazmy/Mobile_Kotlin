f503efaa856cffbfe8f54ce161bd56ffbd184b23
aqnaazmy <aqnaazmy@gmail.com>
2024-10-17 18:46:47 +0700

    mobile

---
+++ src/week1/DataType.kt
@@ -0,0 +1,26 @@
+package week1
+
+fun main() {
+
+    //Tipe data String
+    val nama : String = "plugin"
+    // nama = "name"
+
+    //Tipe data Int
+    val tahun = 2017
+
+    val result = "nama $nama tahun $tahun"
+
+    println(result)
+
+    //Tipe data double
+    val double = 2.3
+
+    //tipe data Char
+    val char = 'A'
+
+    //Tipe data boolean
+    val benar = true
+    val salah = false
+
+}
